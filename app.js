50315a8d01df10764909f9849933cf5dab4ad5f3
PaulaRaventos <pau_89mdp@hotmail.com>
2023-05-20 15:35:26 -0300

    corriendo host

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express()
 const publicPath = path.resolve(__dirname, "./public")
 app.use (express.static(publicPath))
 
-app.listen(3000, () =>{
-    console.log("Servidor corriendo en el puerto 3000")
+app.listen(3002, () =>{
+    console.log("Servidor corriendo en el puerto 3002")
 })
 
 app.get("/login", (req, res) =>{
