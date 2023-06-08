da76c8aef985158b2eb8df2559bf3727b3554b87
julicarlomagno <julicarlomagno@gmail.com>
2023-06-08 15:17:19 -0300

    carrito dinámico

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,12 +1,31 @@
 const path = require('path');
 
+const prodCart = [
+    {
+        id: 1,
+        foto: "buzo_blend.jpg",
+        nombre: "Buzo Blend",
+        talle: 'M',
+        color: 'Negro',
+        precio: 8290
+    },
+    {
+        id: 2,
+        foto: "crop_top.jpg",
+        nombre: "Polera lanilla",
+        talle: 'S',
+        color: 'Negro',
+        precio: 5490
+    },
+]
+
 const controller = {
 
     home: (req,res) =>{
         res.sendFile(path.resolve('./views/home.html'))
     },
     carrito: (req,res) =>{
-        res.render('productCart')
+        res.render('productCart', {cart: prodCart})
     }
 };
 
