93485ecae3642f795e0f8bf398f70a5ededcd946
Xime Espagnol <ximenaespagnol@gmail.com>
2023-06-16 22:33:12 -0300

    productjson multer y method override seteados

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 const path = require ('path')
 const express = require("express")
-
 const app = express()
-
-/*const publicPath = path.resolve(__dirname, "./public")*/
-app.use(express.static('public'))
-
-app.set('views', path.join(__dirname, '../views'));
-app.set ('view engine', 'ejs')
 
 const mainRouter = require ('./routes/mainRouter');
 const productRouter = require ('./routes/productRouter');
 const userRouter = require ('./routes/userRouter');
-/*fijarse de llamar a la carpeta router*/
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+app.set('views', path.join(__dirname, '../views'));
+app.set ('view engine', 'ejs')
+
 
 app.listen(3002, () =>{
     console.log("Servidor corriendo en el puerto 3002")
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,17 +1,39 @@
+const path = require('path')
 const express = require("express");
-
 const router = express.Router();
+const multer= require('multer');
 
 const controller = require('../controllers/productController');
 
+const storage= multer.diskStorage({
+    destination: (req, file,cb)=>{
+        cb(null, path.join(__dirname, '../../public/img'))
+    },
+    filename: (req, file, cb)=>{
+        let imageName = Date.now() + path.extname(file.originalname)
+        cb(null, imageName)
+    }
+})
+
+const fileUpload = multer({
+    storage: storage
+})
+
+
+//
 router.get('/product/:id', controller.product);
-
-router.get('/categorias', controller.categorias);
-
+router.get('/products', controller.categorias);
 router.get('/probador', controller.probador);
 
+//FORM CREATE
 router.get('/altaProducto', controller.altaProducto)
 
+
+//FORM EDIT
 router.get('/modificar/:id', controller.modifProducto)
+
+
+//FORM DELETE
+
 
 module.exports = router
