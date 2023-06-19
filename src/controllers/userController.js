68dc648140dd3997fe913ea963553c7fe8dd53c7
PaulaRaventos <pau_89mdp@hotmail.com>
2023-06-19 17:18:41 -0300

    agregando en userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const usersId = JSON.parse(fs.readFileSync(path.resolve('./src/database/users.json')))
 
 const controller = {
 
@@ -8,8 +9,13 @@ const controller = {
     register: (req,res) =>{
        return res.render ('register')
     },
-    
-    }
+
+    users: (req, res) => {
+      const userFinded = usersId.find(row => row.id == req.params.id)
+      if (userFinded) return res.render('userFinded', { users: userFinded })
+      else return res.send("Para poder ingresar, debe registrarse")
+    },
+   }
 
 
 module.exports = controller;
